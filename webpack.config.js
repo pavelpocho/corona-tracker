00242ab31b933479d139cac4c3aaf9535a106a76
Pavel Pochobradský <Pochobradsky@porg.cz>
2020-03-22 00:20:56 +0100

    More countries

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     fs: "empty"
   },
   output: {
-    filename: "bundle_0.2.js",
+    filename: "bundle_0.3.js",
     chunkFilename: "[name].bundle.js",
     publicPath: "/dist/",
     path: path.resolve("C:/xampp/htdocs/corona-tracker/dist")
@@ -19,7 +19,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "style_0.2.css",
+      filename: "style_0.3.css",
       chunkFilename: "something.css"
     })
   ],
